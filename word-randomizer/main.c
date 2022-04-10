ab60d40d4baef86703be8ca196e2e2181ffc18d2
andrewstephengames <andrewstephengames999@gmail.com>
2022-04-10 19:40:26 +0300

    Add -u flag for using commented out words in input file.

--- word-randomizer/main.c
+++ word-randomizer/main.c
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <string.h>
 
-#define WORD_COUNT 100
+#define WORD_COUNT 500
 #define MAX_LEN 20
 #define PI 3.1415926535897
 
@@ -30,6 +30,7 @@ int main (int argc, char **argv)
 {
      srand (clock());
      size_t randSize = 0, outputSignal = 0, appendSignal = 0;
+     size_t commentSignal = 0;
      char outputFilename[255] = "";
      char words[WORD_COUNT][MAX_LEN];
      FILE *in = fopen ("words.txt", "r"), *out;
@@ -38,22 +39,30 @@ int main (int argc, char **argv)
           switch (argv[1][1])
           {
                case 'o':
+                    //output in a user defined file
                     outputSignal = 1;
                     strcpy (outputFilename, argv[2]);
                     break;
                case 'h':
+                    //print helpful information and usage
                     printUsage ();
                     exit (EXIT_SUCCESS);
                     break;
                case 'a':
+                    //append to existing file on multiple executions
                     appendSignal = 1;
                     break;
                case 'r':
+                    //sentence size
                     randSize = atoi (argv[2]);
                     break;
+               case 'u':
+                    //use words that start with '#' from the input file
+                    commentSignal = 1;
+                    break;
                default:
-                    printf ("\nInvalid argument!\n");
-                    exit (EXIT_SUCCESS);
+                    printf ("Invalid argument!\n");
+                    exit (EXIT_FAILURE);
                     break;
           }
           ++argv;
@@ -77,13 +86,15 @@ int main (int argc, char **argv)
                for (size_t i = 0; i < randSize; i++)
                     if (words[rand()%WORD_COUNT][0] == '#')
                     {
-                         fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
-                         printf ("\n%s\n", outputFilename);
+                         if (commentSignal)
+                              fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
+                         else continue;
+                         //printf ("\n%s\n", outputFilename);
                     }
                     else 
                     {
                          fprintf (out, "%s ", words[rand()%WORD_COUNT]);
-                         printf ("\n%s\n", outputFilename);
+                         //printf ("\n%s\n", outputFilename);
                     }
           }
           else 
@@ -91,13 +102,15 @@ int main (int argc, char **argv)
                for (size_t i = 0; i < randSize; i++)
                     if (words[rand()%WORD_COUNT][0] == '#')
                     {
-                         fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
-                         printf ("\n%s\n", outputFilename);
+                         if (commentSignal)
+                              fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
+                         else continue;
+                         //printf ("\n%s\n", outputFilename);
                     }
                     else 
                     {
                          fprintf (stdout, "%s ", words[rand()%WORD_COUNT]);
-                         printf ("\n%s\n", outputFilename);
+                         //printf ("\n%s\n", outputFilename);
                     }
           }
      }
@@ -107,22 +120,26 @@ int main (int argc, char **argv)
                if (strcmp (outputFilename, "stdout") == 0)
                {
                     if (words[rand()%WORD_COUNT][0] == '#')
-                         continue;
- //                        fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
+                    {
+                         if (commentSignal)
+                              fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
+                         else continue;
+                    }
                     else 
                     {
-                         printf ("\n%s\n", outputFilename);
+                         //printf ("\n%s\n", outputFilename);
                          fprintf (stdout, "%s ", words[rand()%WORD_COUNT]);
                     }
                }
                else 
                {
                     if (words[rand()%WORD_COUNT][0] == '#')
-                         continue;
-//                         fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
+                         if (commentSignal)
+                              fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
+                         else continue;
                     else 
                     {
-                         printf ("\n%s\n", outputFilename);
+                         //printf ("\n%s\n", outputFilename);
                          fprintf (out, "%s ", words[rand()%WORD_COUNT]);
                     }
                }

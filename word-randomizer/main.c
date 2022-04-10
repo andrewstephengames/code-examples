d6fd446ab598e8ba5f062d756751eb89e62612a8
andrewstephengames <andrewstephengames999@gmail.com>
2022-04-10 10:13:51 +0300

    Add Examples; Append Mode; Multiple Option Capability

--- word-randomizer/main.c
+++ word-randomizer/main.c
@@ -1,3 +1,5 @@
+//TODO: custom input file
+//TODO: Fix output in custom file with multiple switches
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -7,34 +9,32 @@
 #define MAX_LEN 20
 #define PI 3.1415926535897
 
-void aboutInfo ()
-{
-     printf ("\nWord Randomizer\n");
-     printf ("\n");
-     printf ("Author: Andrew Stephen\n");
-     printf ("\n");
-     printf ("This program was made to challenge my coding.\n");
-}
-
 void printUsage ()
 {
      printf ("\nUsage:\n");
      printf ("-o: output file\n");
-     printf ("-a: print about info and exit\n");
+     printf ("-a: append mode (requires -o): Appends text to the output file on repeated execution\n");
      printf ("-r: number of words to output\n");
      printf ("-h: print usage and exit\n");
      printf ("\nDefaults: -r variable -o stdout\n\n");
+     printf ("\nExamples:\n");
+     printf ("./main: Prints a variable number of words from 1 to WORD_COUNT from words.txt\n");
+     printf ("./main -r 5: Prints 5 random words from words.txt\n");
+     printf ("./main -r 15 -o poem.txt: Adds 15 random words to poem.txt\n");
+     printf ("On repeated execution, the file is cleared, thus only one sentence is included at one time\n");
+     printf ("./main -o poem.txt -a: Appends a variable number of words to poem.txt\n");
+     printf ("On repeated execution, the file will contain more sentences\n\n");
 }
 
 int main (int argc, char **argv)
 {
      srand (clock());
-     size_t randSize = 0, outputSignal = 0;
+     size_t randSize = 0, outputSignal = 0, appendSignal = 0;
      char outputFilename[255] = "";
      char words[WORD_COUNT][MAX_LEN];
      FILE *in = fopen ("words.txt", "r");
      FILE *out;
-     if ((argc > 1) && (argv[1][0] == '-'))
+     while ((argc > 1) && (argv[1][0] == '-'))
      {
           switch (argv[1][1])
           {
@@ -47,8 +47,7 @@ int main (int argc, char **argv)
                     exit (EXIT_SUCCESS);
                     break;
                case 'a':
-                    aboutInfo ();
-                    exit (EXIT_SUCCESS);
+                    appendSignal = 1;
                     break;
                case 'r':
                     randSize = atoi (argv[2]);
@@ -57,43 +56,55 @@ int main (int argc, char **argv)
                     printf ("\nInvalid argument!\n");
                     break;
           }
+          ++argv;
+          --argc;
      }
      for (size_t i = 0; i < WORD_COUNT; i++)
           fscanf (in, "%s", words[i]);
      if (strlen (outputFilename) == 0)
           strcpy (outputFilename, "stdout");
      if (outputSignal)
-          out = fopen (outputFilename, "w");
+     {
+          if (appendSignal)
+               out = fopen (outputFilename, "a");
+          else 
+               out = fopen (outputFilename, "w");
+     }
      if (randSize)
      {
-          for (size_t i = 0; i < randSize; i++)
-               if (strcmp (outputFilename, "stdout") == 0)
-               {
+          if (strcmp (outputFilename, "stdout") != 0)
+          {
+               for (size_t i = 0; i < randSize; i++)
+                    if (words[rand()%WORD_COUNT][0] == '#')
+                         fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
+                    else 
+                         fprintf (out, "%s ", words[rand()%WORD_COUNT]);
+          }
+          else 
+          {
+               for (size_t i = 0; i < randSize; i++)
                     if (words[rand()%WORD_COUNT][0] == '#')
                          fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
                     else 
                          fprintf (stdout, "%s ", words[rand()%WORD_COUNT]);
-               }
-               else 
-               {
-                    if (words[rand()%WORD_COUNT][0] == '#')
-                         fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
-               }
+          }
      }
      else
      {
-          for (int i = 0; i < rand()%(WORD_COUNT-1); i++)
+          for (int i = 0; i < rand()%(WORD_COUNT); i++)
                if (strcmp (outputFilename, "stdout") == 0)
                {
                     if (words[rand()%WORD_COUNT][0] == '#')
-                         fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
+                         continue;
+ //                        fprintf (stdout, "%s ", words[rand()%WORD_COUNT]+1);
                     else 
                          fprintf (stdout, "%s ", words[rand()%WORD_COUNT]);
                }
                else 
                {
                     if (words[rand()%WORD_COUNT][0] == '#')
-                         fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
+                         continue;
+//                         fprintf (out, "%s ", words[rand()%WORD_COUNT]+1);
                     else 
                          fprintf (out, "%s ", words[rand()%WORD_COUNT]);
                }
